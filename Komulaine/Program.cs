12226f476eea040baf09138f057de756119a58b6
Sampsoni <sami.repo@live.com>
2021-07-26 10:01:59 +0300

    Initial commit

---
+++ Komulaine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Komulaine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
